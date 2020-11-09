d77ee21e7b7eceef134a836afe93e77978460104
sebakolacz <70404904+sebakolacz@users.noreply.github.com>
2020-11-09 23:47:24 +0100

    second test

---
+++ src/Wrapper/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+    function Navigation() {
+        return (
+          <nav className={navigation}>
+            <div className={navigation__logo}>
+              <div className={navigation__image}></div>
+              <div className={navigation__name}></div>
+            </div>
+          </nav>
+        );
+      }
+
+
+
+export default Navigation;
